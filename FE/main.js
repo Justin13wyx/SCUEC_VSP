753ec83062b9684b9f19a196881cdffcacd8defb
Justin13wyx <justin13wyx@gmail.com>
2018-04-02 00:13:11 +0800

    更改用户登录细节, 输入框在toggle时会被清空, 更加安全, TODO:登出确认

--- FE/main.js
+++ FE/main.js
@@ -22,12 +22,18 @@
 
 	var user_mask = document.getElementById("user_mask")
 	var login_panel = document.getElementById("login_panel");
+	var action_switch = document.getElementById("action_switch");
+	var login_part = document.getElementsByClassName("login_part")
+	var login = document.getElementsByClassName("login");
+	var greeting = document.getElementById("greeting");
+	var submit_login = document.getElementById("submit_login");
 	var mask = document.getElementById("mask");
 	var canvas = document.getElementById("loading_token");
 	var context = canvas.getContext('2d');
 	var loading_timer = 0;
 	var start_angle = 0;
 	var access = 0;
+	var username = "";
 
 	// TODO: 如果不支持let关键字时间绑定就会全部失效
 	// 注: 正常情况下, Win10应该没问题, 怕的是Win7啊
@@ -52,10 +58,24 @@
 		else btn.onclick = e => { alert("操作未定义.") }
 	}
 
+	for ( let btn of action_switch.children ) {
+		let role = btn.getAttribute("data-role");
+		btn.addEventListener("click", e => {
+			e.preventDefault();
+			panel_switch(role);
+			for ( let btn of action_switch.children ) {
+				btn.removeAttribute("class")
+			}
+			btn.setAttribute("class", "current_action")
+		})
+	}
+
 	user_mask.addEventListener("click", e => {
 		if (!access) toggle_login(1)
-		else return // 召唤出用户的个人页面
+		else logout() // 登录之后改为登出
 	})
+
+	submit_login.onclick = do_login_or_register
 
 	video.ontimeupdate = function () {
 		if (max_viewtime < video.currentTime) {
@@ -125,6 +145,12 @@
 		}
 	}
 
+	function panel_switch(role) {
+		for ( let part of login_part ) {
+			part.style["display"] = role == "login" ? "none" : "block"
+		}
+	}
+
 	function btn_switch(selected_btn) {
 		for ( let btn of nav_btns ) {
 			btn.setAttribute("class", "nav_btn");
@@ -146,18 +172,22 @@
 		if (method === "POST") {
 			xhr.setRequestHeader("Content-Type", 'application/x-www-form-urlencoded');
 		}
-		xhr.send(transform(data));
+		xhr.send(data);
+		toggle_loading(1)
 		xhr.ontimeout = function (e) {
+			toggle_loading(0)
 			alert("错误(000T)! 请求超时,请检查网络连接.");
 			//TODO: 作进一步处理
 		};
 		xhr.onerror = function (e) {
+			toggle_loading(0)
 			alert("出现错误(000U)! 技术人员请参考控制台输出.")
 			console.log(xhr.status + "<->" + xhr.statusText);
 		};
 		xhr.onreadystatechange = function () {
 			if (xhr.readyState == 4 && xhr.status == 200) {
 				//成功fetch到数据
+				toggle_loading(0)
 				try {
 					var res = JSON.parse(xhr.response);
 				} catch (e) {
@@ -168,8 +198,113 @@
 		};
 	}
 
-	function transform(raw_data) {
+	function do_login_or_register(abs_login) {
+		role = abs_login || document.getElementsByClassName("current_action")[0].getAttribute("data-role")
+		form = new FormData()
+		if (role == 'register') {
+			// 填充验证
+			for ( let i = 1; i < login_panel.childElementCount - 1; i++) {
+				if (login_panel.children[i].value == "") {
+					alert(login_panel.children[i].placeholder+"不能为空");
+					delete form
+					return;
+				}
+				form.set(login_panel.children[i].name, login_panel.children[i].value)
+			}
+			// 密码验证
+			if ( login_part[2].value != login[1].value ) {
+				alert("两次输入的密码不一致");
+				login_part[2].value = "";
+				login[1].value = "";
+				return;
+			}
+			form.delete("passwd_validate");
+			// 注册信息组装
+			data = make_data(form)
+			// 发送注册请求
+			fetch_data("POST", "http://127.0.0.1:5000/apiv1/user/doSignin", check_register_state, data)
+		}
+		else {
+			// 填充验证
+			for ( let ele of login ) {
+				if (ele.value == "") {
+					alert(ele.placeholder+"不能为空");
+					return;
+				}
+				form.set(ele.name, ele.value)
+			}
+			// 登录信息组装
+			data = make_data(form)
+			// 发送登录请求
+			fetch_data("POST", "http://127.0.0.1:5000/apiv1/user/doLogin", check_login_state, data)
+		}
+	}
 
+	function check_register_state(res) {
+		if (res['code'] == '0') {
+			// 注册成功
+			do_login_or_register("login")
+		}
+		if (res['code'] == '-1') {
+			// 后台数据库写入失败
+			alert("注册失败! 后台数据写入异常!")
+		}
+		toggle_login(0)
+	}
+
+	function check_login_state(res) {
+		if (res['code'] == '0') {
+			// 登陆成功
+			username = res['username']
+			greeting.innerHTML = "欢迎," + res['username']
+			access = 1
+			toggle_login(0)
+		}
+		if (res['code'] == '1') {
+			// 密码错误
+			alert("登录失败! 密码错误!")
+		}
+		if (res['code'] == '2') {
+			// 用户不存在
+			alert("登录失败! 用户不存在!")
+		}
+		if (res['code'] == '3') {
+			// 用户账户被冻结
+			alert("登录失败! 用户账户被冻结, 请联系管理员激活!")
+		}
+	}
+
+	function check_logout_state(res) {
+		if (res['code'] == "0") {
+			greeting.innerHTML = "请先登录!"
+			alert("登出成功!")
+			access = 0
+		}
+		else {
+			alert("登出失败! 请稍后尝试.")
+		}
+	}
+
+	function logout() {
+		data = "username=" + username
+		fetch_data("POST", "http://127.0.0.1:5000/apiv1/user/doLogout", check_logout_state, data)
+	}
+
+	/**
+	 * FormData转换函数, 返回字符串
+	 * @param  {[type]} form [description]
+	 * @return {[type]}      [description]
+	 */
+	function make_data(form) {
+		data = ""
+		for ( let entry of form.entries() ) {
+			data += entry[0]
+			data += "="
+			data += entry[1]
+			data += "&"
+		}
+		data = data.substr(0, data.length-1)
+		return data
 	}
 
 	/**
@@ -182,6 +317,7 @@
 	function menu_switch(e, selected_btn, toView, callback) {
 		if (!access) {
 			alert("你还没有登录");
+			toggle_login(1)
 			return;
 		}
 		e.cancelBubble = true;
@@ -230,6 +366,13 @@
 			login_panel.style['transform'] = "translate3d(0, -150%, 0)"
 			mask.removeEventListener("click", e => { toggle_login(0) })
 		}
+		clear_login()
+	}
+
+	function clear_login() {
+		for ( let i = 1; i < login_panel.childElementCount - 1; i++) {
+			login_panel.children[i].value = ""
+		}
 	}
 
 	/**
@@ -251,7 +394,5 @@
 		start_angle %= Math.PI * 2 // 防止数值太大
 		loading_timer = requestAnimationFrame(render_loading)
 	}
-
-	// console.log(PDFJS)
 
 })()
